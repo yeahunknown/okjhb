a2e388c0396fa9e880e7d4268ef8a77459209232
yeahunknown <webrobloxreset@gmail.com>
2025-05-26 03:19:02 -0400

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="trustpilot-one-time-domain-verification-id" content="8240dd3f-703a-4d69-bd42-b5d1b6e43c4a" />
+    </>
+  );
+}
