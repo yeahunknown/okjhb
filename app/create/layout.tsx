a6a42f9c73d8cd297e1967f5d52b68c8f337e47e
yeahunknown <webrobloxreset@gmail.com>
2025-05-25 22:16:41 -0400

    Update layout.tsx

--- app/create/layout.tsx
+++ app/create/layout.tsx
@@ -23,7 +23,7 @@ export default function CreateLayout({
                 </div>
               </div>
               <span className="text-xl font-bold bg-gradient-to-r from-purple-400 to-cyan-400 bg-clip-text text-transparent">
-                solala.win
+                wavelaunch
               </span>
             </Link>
           </div>
