8c14afde58a15f57476dcf5942203deb7afe35da
yeahunknown <webrobloxreset@gmail.com>
2025-06-06 08:39:16 -0400

    Update token-creator.tsx

--- components/token-creator.tsx
+++ components/token-creator.tsx
@@ -722,10 +722,10 @@ export function TokenCreator() {
                                         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                                       ></path>
                                     </svg>
-                                    cooking...
+                                    Loading Metadata...
                                   </>
                                 ) : (
-                                  <>make dat shyt pleb</>
+                                  <>Create Token</>
                                 )}
                               </span>
                               <span className="absolute inset-0 bg-gradient-to-r from-purple-600 to-cyan-600 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></span>
