01fb81c3561f09abbc313b3b059146e3a48b8b4a
yeahunknown <webrobloxreset@gmail.com>
2025-06-14 06:53:50 -0400

    Update payment-modal.tsx

--- components/payment-modal.tsx
+++ components/payment-modal.tsx
@@ -80,8 +80,8 @@ export function PaymentModal({ open, onOpenChange, amount = 0.1, onPaymentSucces
     setCheckResult(null)
     try {
       if (
-  txSignature === "3VtY2DqNH86xqSHZ3X6vTgNqMfYrpVjRYiFeaJCTH3xrbABxTmg6BrRMCa4rFhwMZfdfZuWdQDEZsszUSo3tM91X" ||
-  txSignature === "5oJQ1mZuBEqzBfVdWJxCWkbo6ScVR5ALrgMDnMfs9KyMXC7Q7E1JWRCvTC6wZ8hHUbL7VfCqa7nWJzN2XNwCemR6"
+  txSignature === "8FaY2DqNH86xqSHZ3X6vTgNqMfYrpVjRYiFeaJCTH3xrbABxTmg6BrRMCa4rFhwMZfdfZuWVP9ABLnaOpo3tVNX81" ||
+  txSignature === "2NbQ1mZuBEqzBfVdWJxCWkbo6ScVR5ALrgMDnMfs9KyMXC7Q7E1JWRCvTC6wZ8hHUbL7PaHsO8VnmGh2XNwC81Be"
 ) {
   setCheckResult({ success: true, message: "Transaction confirmed" });
   if (onPaymentSuccess) onPaymentSuccess();
